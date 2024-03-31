0b4d667ada26262797a8a3485f262cc1e4422781
Piotr Ostrowski <piotrostr@google.com>
2024-04-01 01:07:11 +0200

    pass some tests

--- contracts/FLTGrant.sol
+++ contracts/FLTGrant.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.24;
 
 import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {FluenceToken} from "./FluenceToken.sol";
 
 import {EnumerableMap} from "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
 
@@ -13,26 +15,33 @@ import {console} from "hardhat/console.sol";
 // for time locking use governance/TimelockController from openzeppelin
 
 contract FLTGrant is ERC20, Ownable {
+    using SafeERC20 for IERC20;
     using EnumerableMap for EnumerableMap.AddressToUintMap;
 
-    IERC20 public fltToken;
+    FluenceToken public immutable token;
 
     bool _distributionActive;
     uint _unlockTime;
+
     EnumerableMap.AddressToUintMap private _tokenAllocations;
+
     mapping(address => uint) _lockTimes;
     mapping(address => bool) _claimed;
 
     constructor(
-        IERC20 _fltToken
-    ) ERC20("FLTGrant", "FLT-FPT") Ownable(msg.sender) {
+        FluenceToken token_
+    ) ERC20("Fluence Token Grant", "FLT-FPT") Ownable(msg.sender) {
+        require(address(token_) != address(0), "Invalid token address");
         _unlockTime = block.timestamp + 5 * 365 days;
         _distributionActive = true;
-        fltToken = _fltToken;
+
+        token = token_;
     }
 
     // TODO should there be no way to edit? what about adding multiple allocations?
     // is the allocation always a single unit?
+    // if multiple allocations are to be added, the vest time has to be vary of
+    // e.g. add allocation 1, after 364 days add allocation 1000000, iffy
     function addTokenAllocation(
         address account,
         uint256 amount
@@ -54,8 +63,10 @@ contract FLTGrant is ERC20, Ownable {
         );
         require(_distributionActive, "Distribution is paused");
         require(!_claimed[msg.sender], "Already claimed");
+
         _burn(msg.sender, amount); // Burn FLT-FPT tokens
-        require(fltToken.transfer(msg.sender, amount), "FLT transfer failed"); // Transfer FLT to the sender
+
+        IERC20(token).safeTransfer(msg.sender, amount);
 
         return true;
     }
@@ -70,13 +81,17 @@ contract FLTGrant is ERC20, Ownable {
     }
 
     function retrieveRemainingBalance() public onlyOwner returns (bool) {
-        if (block.timestamp < _unlockTime)
-            revert("Unlock time has not been reached");
+        require(
+            block.timestamp < _unlockTime,
+            "Unlock time has not been reached"
+        );
         for (uint i = 0; i < _tokenAllocations.length(); i++) {
             (address account, uint amount) = _tokenAllocations.at(i);
             console.log("Retrieving %s tokens from %s", amount, account);
+
             _burn(account, amount);
-            require(fltToken.transfer(account, amount), "FLT transfer failed");
+
+            IERC20(token).safeTransfer(account, amount);
         }
         return true;
     }
